239015d510728a8270b4fb8661c779ef3e684a1e
Alistair Blake <consultations@me.com>
2020-01-29 21:22:43 -0800

    working app. Will next deploy to AWS.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/header/header.js';
+import Counter from './components/counter/counter.js';
+import './app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+        <React.Fragment>
+          <Header />
+          <Counter />
+        </React.Fragment>
+    );
+  }
+}
+
+export default App;
